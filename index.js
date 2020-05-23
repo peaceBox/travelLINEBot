aa5536c9abdc506fde93a8c485047556fcaaad48
youkan2002 <inoueyosuke2002@gmail.com>
2020-05-23 23:16:11 +0900

    おうむ返し成功

---
+++ index.js
@@ -0,0 +1,70 @@
+"use strict";
+// モジュール呼び出し
+const crypto = require("crypto");
+const line = require("@line/bot-sdk");
+
+// インスタンス生成
+const client = new line.Client({ channelAccessToken: process.env.ACCESSTOKEN });
+
+exports.handler = (event, context,) => {
+  // 署名検証
+  const signature = crypto
+    .createHmac("sha256", process.env.CHANNELSECRET)
+    .update(event.body)
+    .digest("base64");
+  const checkHeader = (event.headers || {})["X-Line-Signature"];
+  const body = JSON.parse(event.body);
+  const events = body.events;
+  console.log(events);
+
+  // 署名検証が成功した場合
+  if (signature === checkHeader) {
+    events.forEach(async (event) => {
+      let message;
+      // イベントタイプごとに関数を分ける
+      switch (event.type) {
+        // メッセージイベント
+        case "message":
+          message = await messageFunc(event);
+          break;
+        // フォローイベント
+        case "follow":
+          message = { type: "text", text: "追加ありがとうございます！" };
+          break;
+        // ポストバックイベント
+        case "postback":
+          message = await postbackFunc(event);
+          break;
+      }
+      // メッセージを返信
+      if (message != undefined) {
+        client
+          .replyMessage(body.events[0].replyToken, message)
+          .then((response) => {
+            const lambdaResponse = {
+              statusCode: 200,
+              headers: { "X-Line-Status": "OK" },
+              body: '{"result":"completed"}',
+            };
+            context.succeed(lambdaResponse);
+          })
+          .catch((err) => console.log(err));
+      }
+    });
+  }
+  // 署名検証に失敗した場合
+  else {
+    console.log("署名認証エラー");
+  }
+};
+
+const messageFunc = async function (event) {
+  let message;
+  message = { type: "text", text: `メッセージイベントを受け付けました！` };
+  return message;
+};
+const postbackFunc = async function (event) {
+  let message;
+  message = { type: "text", text: "ポストバックイベントを受け付けました！" };
+  return message;
+};
