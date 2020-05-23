7b829f43500b9e2daffae41040a3029b2a3e37ad
youkan2002 <inoueyosuke2002@gmail.com>
2020-05-24 05:24:45 +0900

    テスト

--- handler.js
+++ handler.js
@@ -50,7 +50,9 @@ module.exports.hello = (event, context, ) => {
           message = await postbackFunc(event);
           break;
         case 'join':
+           await  getTravelId(event)
           message = [join,{type:"text",text:event.source.groupId}];
+
           break;
       }
       // メッセージを返信
@@ -147,3 +149,31 @@ const postbackFunc = async function (event) {
   };
   return message;
 };
+
+
+const getTravelId = async function (event){
+  let params = {
+    FunctionName: `travel-lambda-dev-hello`,
+    InvocationType: "RequestResponse",
+    Payload: JSON.stringify({
+      type: "lambda",
+      path: "/travel",
+      httpMethod: "POST",
+      body: JSON.stringify( { 
+         groupId: "XXxxxxxX" 
+          })
+    }),
+  };
+
+  JSON.stringify();
+  // 呼び出される側のLambda関数を実行する
+  let result = await lambda.invoke(params).promise(); //おじさん呼びに行って返ってくるまで待つ
+let res = JSON.parse(result.Payload).body
+let  res2 = JSON.parse(res)
+console.log(res2.travelId)
+
+  // 呼び出される側のLambda関数を実行する
+  let result = await lambda.invoke(params).promise(); //おじさん呼びに行って返ってくるまで待つ
+ console.log(`travelID:${result}`)
+
+}
